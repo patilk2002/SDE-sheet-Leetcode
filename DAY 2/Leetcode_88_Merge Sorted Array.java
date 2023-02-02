63097f7f34374e43bca72abef516675a535bf050
Kiran <kiran.kp2004@gmail.com>
2023-02-02 23:50:13 +0530

    Day 3

---
+++ DAY 2/Leetcode_88_Merge Sorted Array.java
@@ -0,0 +1,22 @@
+class Solution {
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        int index = nums1.length -1;
+        
+        while(m-1>=0 && n-1>=0){
+            
+            if(nums1[m-1] >= nums2[n-1]){
+                nums1[index--] = nums1[m-1];
+                m--;
+            }else{
+                nums1[index--] = nums2[n-1];
+                n--;
+            }
+        }
+        
+        while(n-1>= 0){
+            nums1[index--] = nums2[n-1];
+            n--;
+        }
+    }
+}
